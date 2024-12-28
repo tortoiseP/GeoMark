77885ed5227d0b2e1ca430e24e2a3befeccb4fb6
xupei <xupei2000@foxmail.com>
2024-12-28 17:50:36 +0800

    init: 初始化地貌图语料标注后端项目

---
+++ geomark-be/src/main/java/com/windflowers/geomarkbe/GeomarkBeApplication.java
@@ -0,0 +1,13 @@
+package com.windflowers.geomarkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomarkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeomarkBeApplication.class, args);
+    }
+
+}
---
+++ geomark-be/src/test/java/com/windflowers/geomarkbe/GeomarkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windflowers.geomarkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeomarkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
